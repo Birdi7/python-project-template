4d9b1fd6a706b3554ea3a80a68ce3c7ebd5fd39f
Egor Osokin <egor.birdi7@gmail.com>
2019-12-14 18:14:40 +0300

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for i in "$@"; do
+    case $i in
+    -n=* | --name=*)
+        DIR_NAME="${i#*=}"
+        ;;
+    esac
+done
+
+git clone --progress git@github.com:Birdi7/python-project-template.git ${DIR_NAME}
