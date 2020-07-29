611180a6d99b1c5269f771efb8234c837e23c0c6
Egor <egor.osokin7@gmail.com>
2020-07-29 16:37:43 +0300

    fix: remove .git directory in installing

--- install.sh
+++ install.sh
@@ -8,5 +8,6 @@ done
 
 git clone --progress git@github.com:Birdi7/python-project-template.git ${DIR_NAME} &&
     cd ${DIR_NAME} &&
+    rm -rf .git && 
     poetry install &&
     poetry run pre-commit install
